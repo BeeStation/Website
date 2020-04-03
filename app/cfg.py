952de871a59fc6014daa26822875721ab36d7b3e
qwerty <qwertytrogi@gmail.com>
2020-04-03 17:21:02 -0500

    Fix YAML load warnings

--- app/cfg.py
+++ app/cfg.py
@@ -1,6 +1,6 @@
 import yaml
 
-WEBSITE = yaml.load(open("app/config/website.yml"))
-SERVERS = yaml.load(open("app/config/servers.yml"))
-MAPS = yaml.load(open("app/config/maps.yml"))
-PRIVATE = yaml.load(open("app/config/private.yml"))
+WEBSITE	= yaml.load(open("app/config/website.yml"),	Loader=yaml.SafeLoader)
+SERVERS	= yaml.load(open("app/config/servers.yml"),	Loader=yaml.SafeLoader)
+MAPS	= yaml.load(open("app/config/maps.yml"),	Loader=yaml.SafeLoader)
+PRIVATE	= yaml.load(open("app/config/private.yml"),	Loader=yaml.SafeLoader)
