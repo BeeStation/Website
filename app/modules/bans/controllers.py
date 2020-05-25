e118fdcb653127ebb6aa7b4fd260f593d913ffb9
qwerty <qwertytrogi@gmail.com>
2020-05-25 14:32:04 -0500

    json ban fetching

--- app/modules/bans/controllers.py
+++ app/modules/bans/controllers.py
@@ -3,6 +3,7 @@ from app import db
 from app import util
 
 from flask import Blueprint
+from flask import jsonify
 from flask import render_template
 from flask import request
 
@@ -15,6 +16,8 @@ def page_bans():
 	page = request.args.get('page', type=int, default=1)
 	query = request.args.get('q', type=str, default="")
 
+	json_format = request.args.get('json', type=int, default=0)
+
 	page = max(min(page, 1000000), 1) # Arbitrary number. We probably won't ever have to deal with 1,000,000 pages of bans. Hopefully..
 
 	response = db.game_db.get_bans((page-1)*cfg.WEBSITE["items-per-page"], page*cfg.WEBSITE["items-per-page"], query.lower().strip())
@@ -22,4 +25,8 @@ def page_bans():
 	bans = response[0]
 	length = response[1]
 	buttons = [page > 1, page < length / cfg.WEBSITE["items-per-page"]]
+
+	if json_format:
+		return jsonify(bans)
+
 	return render_template("bans.html", bans=bans, buttons=buttons, page=page, query=query, pages=math.ceil(length / cfg.WEBSITE["items-per-page"]))
